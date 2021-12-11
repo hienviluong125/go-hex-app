2ce0edbe167bf4ae99b2475f9f4307c8a0358ab6
hienviluong125 <hienviluong125@gmail.com>
2021-12-11 18:40:42 +0700

    feat: add root endpoint handler
    feat: add deploy pipeline

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"hienviluong125/go-hex-app/common"
 	"hienviluong125/go-hex-app/logger"
 	"hienviluong125/go-hex-app/middleware"
 	"hienviluong125/go-hex-app/modules/productmodule"
@@ -29,6 +30,7 @@ func main() {
 
 	r.Use(middleware.HandleErrorMiddleware)
 
+	r.HandleFunc("/", homePageHandler).Methods(http.MethodGet)
 	r.HandleFunc("/products", productHandler.Index).Methods(http.MethodGet)
 	r.HandleFunc("/products", productHandler.Create).Methods(http.MethodPost)
 	r.HandleFunc("/products/{product_id}", productHandler.Show).Methods(http.MethodGet)
@@ -36,4 +38,8 @@ func main() {
 	r.HandleFunc("/products/{product_id}", productHandler.Destroy).Methods(http.MethodDelete)
 	logger.Info("Listening server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	common.RespondWithStatus(w, http.StatusOK, "Welcome to KennyKode")
 }
